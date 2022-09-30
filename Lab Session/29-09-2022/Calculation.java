8ed940c86312d450d291f137f34ba25c7637feb2
ak2255007 <109300273+ak2255007@users.noreply.github.com>
2022-09-30 15:52:31 +0530

    Update Calculation.java

--- Lab Session/29-09-2022/Calculation.java
+++ Lab Session/29-09-2022/Calculation.java
@@ -1,4 +1,12 @@
-/*program for calculation
+/*
+Create a program for calculation.read three values from the user for operation
+first and second values as operands.
+third value as operator.
+if user press1 => print addition of first and second
+if user press2 => print subtraction of first and second
+if user press3 => print multiplication of first and second
+if user press4 => print division of first and second
+
 @auther : Amit kumar
 date : 29-09-22
 */
