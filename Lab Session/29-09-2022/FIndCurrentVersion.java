b9cba525a79cfbe4e636411482208bee53c396c2
ak2255007 <109300273+ak2255007@users.noreply.github.com>
2022-09-30 15:41:04 +0530

    Update FIndCurrentVersion.java

--- Lab Session/29-09-2022/FIndCurrentVersion.java
+++ Lab Session/29-09-2022/FIndCurrentVersion.java
@@ -1,11 +1,14 @@
-// Program to Find the current version in our pc
-
+/* Program to Find the current version in our pc
+@auther : Amit kumar
+date : 29-09-22
+*/
 package CoreJava;
 
 class FIndCurrentVersion {
 	
 	public static void main (String args[])
 	{
+		// for print output
 		System.out.println("version of java is :" +System.getProperty("java.version"));
 	}
 
