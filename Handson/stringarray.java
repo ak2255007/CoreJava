5d4f8b7e624e4dd41973c584471ccada27cdfe17
ak2255007 <109300273+ak2255007@users.noreply.github.com>
2022-10-19 15:23:00 +0530

    Add files via upload

---
+++ Handson/stringarray.java
@@ -0,0 +1,13 @@
+// program to print string array
+// author by Amit kumar
+//19-10-2022
+
+
+
+public class stringarray{
+public static void main(String args[]){
+
+String name[]={"amit","sunil","yogesh","gautam","vishal"};
+System.out.println(name[3]);
+}
+}
